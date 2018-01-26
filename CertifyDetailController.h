8eef9c410eafe440d6925d7a5718b6d2e691729c
legendbeibei <870240120@qq.com>
2018-01-26 16:29:52 +0800

    chen
    hahahah

---
+++ CertifyDetailController.h
@@ -0,0 +1,13 @@
+//
+//  CertifyDetailController.h
+//  SpeedLoan
+//
+//  Created by legend on 2018/1/15.
+//  Copyright © 2018年 legend. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface CertifyDetailController : BaseViewController
+
+@end
